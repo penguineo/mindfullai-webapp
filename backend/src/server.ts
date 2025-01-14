c12f89c163bec7f6fa78b1fdbeca986c85b187b9
penguineo <subham020304@gmail.com>
2025-01-14 14:40:38 +0000

    refactor: portGRPC parsed to string

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { ServerCredentials } from "@grpc/grpc-js";
 import { serverGRPC } from "./api/grpc";
 
-const portGRPC: string = process.env.PORT_GRPC;
+const portGRPC: string = process.env.PORT_GRPC.toString();
 const cred = ServerCredentials.createInsecure();
 
 serverGRPC.bindAsync(`localhost:${portGRPC}`, cred, (error, port) => {
