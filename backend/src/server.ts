378911b70b75d81f904b6edfcf8ae90b11081b5e
penguineo <subham020304@gmail.com>
2025-01-19 05:05:06 +0000

    feat: add trpc server listener
    - Added trpc server listner to start with server.ts.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import { ServerCredentials } from "@grpc/grpc-js";
 import { serverGRPC } from "./api/grpc";
 import { serverHTTP } from "./api/http";
+import { serverTRPC } from "./api/trpc";
 
 const portGRPC: string = process.env.PORT_GRPC.toString();
 const portHTTP: number = process.env.PORT_HTTP;
+const portTRPC: number = process.env.PORT_TRPC;
 const cred = ServerCredentials.createInsecure();
 
 serverGRPC.bindAsync(`localhost:${portGRPC}`, cred, (error, port) => {
@@ -17,3 +19,11 @@ serverGRPC.bindAsync(`localhost:${portGRPC}`, cred, (error, port) => {
 serverHTTP.listen(portHTTP, "localhost", () => {
 	console.log(`HTTP Server Listening on port: ${portHTTP}`);
 });
+
+try {
+	serverTRPC.listen(portTRPC, "localhost");
+	console.log(`tRPC Server Listening on port: ${portTRPC}`);
+} catch (error) {
+	console.error("Error starting tRPC server:", error);
+	process.exit(1);
+}
